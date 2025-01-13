fb19eb8f5ad0884071be54d9f7beed01393bf2c6
Kin Leon Zinzombe <kinzinzombe07@Gmail.com>
2025-01-13 12:04:11 +0200

    [UPDATE]: Implement CORS support in API route for enhanced request handling
    - Added CORS headers to the POST request handler in the generate API route.
    - Implemented OPTIONS method response to facilitate preflight requests.
    - Improved API route functionality to support cross-origin requests.

--- application/src/app/api/generate/route.ts
+++ application/src/app/api/generate/route.ts
@@ -7,6 +7,18 @@ import { getBestKeywords } from "@/lib/getBestKeywords";
 import getSiteSeoData from "@/lib/getSiteSeoData";
 
 export async function POST(req: NextRequest) {
+  // Setup cors
+  const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  };
+
+  if (req.method === "OPTIONS") {
+    return NextResponse.json({}, { headers: corsHeaders });
+  }
+
+    
     // get the formdata from the request
     const {
       url, 
