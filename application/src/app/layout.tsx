58ff4bb1b825dc804fc7cd02bac49edd9401c804
Kin Leon Zinzombe <kinzinzombe07@Gmail.com>
2025-01-13 03:26:16 +0200

    [ADD]: Topbar

--- application/src/app/layout.tsx
+++ application/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Topbar from "@/components/Topbar";
 
 export const metadata: Metadata = {
   title: "aiseogen",
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+        <Topbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ application/src/components/Topbar.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Topbar() {
+  return (
+    <section className="w-full px-2 py-2 md:px-8 md:py-4 flex justify-between items-center border-b-2 border-gray-300">
+        <h2 className="text-2xl font-semibold">AISEOgen</h2>
+
+        <article className="flex items-center justify-center gap-2">
+            <Link className="px-4 py-2 transition duration-300 hover:bg-blue-500 hover:text-white rounded-xl border-2 border-blue-500 text-blue-500" href="mailto:support@aiseogen.com">
+                Report a bug
+            </Link>
+            <Link className="px-4 py-2 transition duration-300 hover:bg-purple-700 rounded-xl bg-purple-500 text-white" href="mailto:support@aiseogen.com">
+                Request a feature
+            </Link>
+        </article>
+    </section>
+  )
+}
